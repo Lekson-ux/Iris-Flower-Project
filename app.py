48b9a905aae3419c6192c9ecb739b105e8cd6505
Usman Oladapo <87792198+Lekson-ux@users.noreply.github.com>
2022-11-15 17:43:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
+import pickle
+import sklearn
+#import streamlit
+import numpy as np
+import pandas as pd
+#loaded_model = pickle.load(open('C:/Users/USER/Documents/Streamlit ML APPS/Diabetes Project/Saved_model.pkl', 'rb'))
+load_model = pickle.load(open('IRIS_model.pkl', 'rb'))
+input_data = (7.0, 3.2, 4.7, 1.4)
+
+# changing the input_data to numpy array
+input_data = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data.reshape(1,-1)
+
+prediction = load_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+    print('Predicted flower is Setosa')
+elif (prediction[0]== 1):
+    print('Predicted flower is Versicolour')
+else:
+    print('Predicted flower is Virginica')
---
+++ webapp.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
+import sklearn
+import pickle
+import numpy as np
+import pandas as pd
+load_model = pickle.load(open('IRIS_model.pkl', 'rb'))
+
+#creating a function for prediction
+def iris_prediction(input_data):
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.array(input_data, dtype='float')
+    #input_data_as_numpy_array = to_numeric(input_data)
+    
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = load_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return ('Predicted flower is Setosa')
+    elif (prediction[0]== 1):
+        return ('Predicted flower is Versicolour')
+    else:
+        return ('Predicted flower is Virginica')
+
+def main():
+    #giving title
+    st.title('Iris Flower Classifier ML Web App')
+    st.subheader('Developer: Usman Oladapo :sunglasses:')
+    st.write("### We need some information to classify flower")
+
+    #getting input from user
+    sepal_length = st.text_input("Sepal Length")
+    sepal_width = st.text_input("Sepal Width")
+    petal_length = st.text_input("Petal Length")
+    petal_width = st.text_input("Petal Width")
+
+    result = ""
+
+
+    #creating a button for prediction
+
+    if st.button('Predict Flower'):
+        result = iris_prediction([sepal_length, sepal_width, petal_length, petal_width])
+    st.success(result)
+
+if __name__ == '__main__':
+    main()
